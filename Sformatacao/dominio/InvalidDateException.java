b22b15818babefa17f4345303d09ed31caea00cb
Carlos Vinicius <cv211107@gmail.com>
2026-05-10 12:43:00 -0300

    feat: Appointment Scheduling mini System

---
+++ Sformatacao/dominio/InvalidDateException.java
@@ -0,0 +1,7 @@
+package maratonajava.javacore.Sformatacao.dominio;
+
+public class InvalidDateException extends Exception {
+    public InvalidDateException(String message) {
+        super(message);
+    }
+}
