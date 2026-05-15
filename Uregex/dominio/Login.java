8c96e7873770e94c6fbc416e543e2531dea54cce
Carlos Vinicius <cv211107@gmail.com>
2026-05-15 15:49:36 -0300

    feat: add to anchor

--- Uregex/dominio/Login.java
+++ Uregex/dominio/Login.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class Login {
 
     public void loginValidation(String email) throws Exception{
-        String regex = "\\w+@gmail(\\.[a-z]{3})$";
+        String regex = "^\\w+@gmail(\\.[a-z]{3})$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         if (!matcher.matches()){
